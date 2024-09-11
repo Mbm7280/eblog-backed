4814ce1f0dd5ac4ea012a21971dbd1250c695a53
Echo <56158949+Mbm7280@users.noreply.github.com>
2024-09-11 08:57:34 +0800

    1、Header Layout Update 2、Premission 3、SiderBar

--- src/permission.js
+++ src/permission.js
@@ -1,3 +1,31 @@
+import router from "@/router"
+import store from '@/store'
+import Nprogress from "nprogress"
+import 'nprogress/nprogress.css'
+
+const whiteList = ['/login', '/404']
+router.beforeEach(function(to, from, next) {
+  Nprogress.start()
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+  Nprogress.done()
+})
+
+router.afterEach(function() {
+  Nprogress.done()
+})
+
 // import router from './router'
 // import store from './store'
 // import { Message } from 'element-ui'
